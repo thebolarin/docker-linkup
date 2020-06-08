c3d02e1361c45aa3c746b3631484ee4cc2e3d8c1
thebolarin <odutusinmoses@gmail.com>
2020-06-08 12:42:07 +0100

    first upload

---
+++ services/node/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const PORT = 3001;
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.get('/api/users', function(req, res) {
+    res.json([
+        "Sally",
+        "Bernie",
+        "Robbie",
+        "Moses",
+    ]);
+});
+
+app.listen(PORT, function() {
+    console.log("Node Server running on PORT:" + PORT);
+});
